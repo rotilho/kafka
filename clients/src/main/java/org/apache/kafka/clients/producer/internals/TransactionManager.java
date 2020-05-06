d4d72838b248e0a8e6434e5b8ebfe0665dee11be
Joost van de Wijgerd <jwijgerd@gmail.com>
2020-05-06 10:23:06 +0200

    TransactionManager now supports caching of multiple consumerGroupCoordinator nodes avoiding the need find coordinators again when multiple consumer groups are used with a single transactional KafkaProducer

--- clients/src/main/java/org/apache/kafka/clients/producer/internals/TransactionManager.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/internals/TransactionManager.java
@@ -237,7 +237,7 @@ public class TransactionManager {
 
     private int inFlightRequestCorrelationId = NO_INFLIGHT_REQUEST_CORRELATION_ID;
     private Node transactionCoordinator;
-    private Node consumerGroupCoordinator;
+    private final Map<String, Node> consumerGroupCoordinator;
     private boolean coordinatorSupportsBumpingEpoch;
 
     private volatile State currentState = State.UNINITIALIZED;
@@ -311,7 +311,7 @@ public class TransactionManager {
         this.log = logContext.logger(TransactionManager.class);
         this.transactionTimeoutMs = transactionTimeoutMs;
         this.transactionCoordinator = null;
-        this.consumerGroupCoordinator = null;
+        this.consumerGroupCoordinator = new HashMap<>();
         this.newPartitionsInTransaction = new HashSet<>();
         this.pendingPartitionsInTransaction = new HashSet<>();
         this.partitionsInTransaction = new HashSet<>();
@@ -925,10 +925,10 @@ public class TransactionManager {
         }
     }
 
-    Node coordinator(FindCoordinatorRequest.CoordinatorType type) {
+    Node coordinator(CoordinatorType type, String coordinatorKey) {
         switch (type) {
             case GROUP:
-                return consumerGroupCoordinator;
+                return consumerGroupCoordinator.get(coordinatorKey);
             case TRANSACTION:
                 return transactionCoordinator;
             default:
@@ -1134,7 +1134,7 @@ public class TransactionManager {
     private void lookupCoordinator(FindCoordinatorRequest.CoordinatorType type, String coordinatorKey) {
         switch (type) {
             case GROUP:
-                consumerGroupCoordinator = null;
+                consumerGroupCoordinator.remove(coordinatorKey);
                 break;
             case TRANSACTION:
                 transactionCoordinator = null;
@@ -1535,7 +1535,7 @@ public class TransactionManager {
                 Node node = findCoordinatorResponse.node();
                 switch (coordinatorType) {
                     case GROUP:
-                        consumerGroupCoordinator = node;
+                        consumerGroupCoordinator.put(builder.data().key(), node);
                         break;
                     case TRANSACTION:
                         transactionCoordinator = node;
